0ba50247032a47d23e00a47a74865269ed834f11
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-07-03 15:58:42 +0300

    The designated location on the map is displayed

---
+++ app/src/main/java/com/example/learngit/MapsActivity.kt
@@ -0,0 +1,38 @@
+package com.example.learngit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.learngit.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val istanbul = LatLng(41.0082, 28.9784)
+        mMap.addMarker(MarkerOptions().position(istanbul).title("Marker in Istanbul"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(istanbul,12f))
+    }
+
+}
