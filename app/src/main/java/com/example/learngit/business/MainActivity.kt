ad2bd540b584629e88cd0817d907a8d0023f1051
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-07-11 11:36:03 +0300

    Feat: Add Fragment Navigation
    Added functionality to navigate to a blank fragment when the text view is clicked.
    This includes setting up an onClickListener and handling
     the fragment transaction.

--- app/src/main/java/com/example/learngit/business/MainActivity.kt
+++ app/src/main/java/com/example/learngit/business/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.learngit.business
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.learngit.R
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.businesses_current_location_text_view)
+        textView.setOnClickListener {
+            loadFragment(BlankFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(android.R.id.content, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
