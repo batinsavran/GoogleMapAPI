1922438175af831c4e9011c8934c1f5454c70f6d
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-07-13 12:08:24 +0300

    Feat: Add filter bottom sheet

--- app/src/main/java/com/example/learngit/customer/ListBusinessActivity.kt
+++ app/src/main/java/com/example/learngit/customer/ListBusinessActivity.kt
@@ -3,7 +3,9 @@ package com.example.learngit.customer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import com.example.learngit.R
 import com.example.learngit.databinding.ActivityListBusinessBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ListBusinessActivity : AppCompatActivity() {
 
@@ -18,5 +20,16 @@ class ListBusinessActivity : AppCompatActivity() {
             val intent = Intent(this, LastActivity::class.java)
             startActivity(intent)
         }
+
+        binding.filter.setOnClickListener {
+            showBottomSheet()
+        }
+    }
+
+    private fun showBottomSheet() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottomsheet_filter, null)
+        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.show()
     }
 }
