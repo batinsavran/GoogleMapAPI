4fb1912b83f6ea2d3c276ae3e3f49a8ed0ae03c0
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-07-17 16:27:35 +0300

    Feat: Add dependencies for networking, analytics, and background tasks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,18 +41,38 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.maps)
-    implementation(libs.play.services.maps)
     implementation(libs.play.services.location)
     implementation(libs.android.maps.utils)
     implementation (libs.google.gson)
+    implementation(libs.androidx.espresso.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.onesignal)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.converter.moshi)
+
+    // OkHttp
+    implementation(libs.logging.interceptor)
+    implementation(libs.okhttp)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.crashlytics.ktx)
+    implementation(libs.firebase.analytics.ktx)
+    implementation(libs.firebase.config.ktx)
+    implementation(libs.firebase.dynamic.links.ktx)
+    implementation(libs.firebase.messaging)
+
+
+    implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.kotlinx.coroutines.android)
+
 }
